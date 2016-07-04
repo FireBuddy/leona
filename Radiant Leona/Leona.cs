de1eddf22c3e7d3e46fc66e1ff0a6c57edefc817
FireBuddy <aijun@live.ca>
2016-07-04 16:14:25 -0400

    Update Leona.cs

--- Radiant Leona/Leona.cs
+++ Radiant Leona/Leona.cs
@@ -125,17 +125,25 @@
             CurrentTarget = TargetSelector.GetTarget(E.Range, DamageType.Magical);
             if (sender == CurrentTarget && !sender.IsDashing() && sender.Type == GameObjectType.AIHeroClient && sender.IsValidTarget(880) && E.IsReady() && sender.IsEnemy)
             {
-                if (args.End.Distance(Player) <= 100)
+                if (args.End.Distance(Player.Instance.ServerPosition) <= 100)
+                {
+                   Chat.Print("Receiving damage"+args.SData.Name);
+
+
+                 }
+                if (args.End.Distance(Player.Instance.ServerPosition) >= 100)
                 {
 
+                    Chat.Print("Not Receiving damage" +args.SData.Name);
+                    SpellManager.Q.Cast(sender.ServerPosition);
 
+                }                
+                if (args.Target != null)
+                {
+                    Chat.Print("targetspell"+args.SData.Name);
+                    SpellManager.Q.Cast(sender.ServerPosition);
 
-                }
-
-
-                
-
-
+}
             } 
 }
 
